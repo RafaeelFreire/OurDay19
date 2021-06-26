c8f8b887386286684a63c4a3ffbab1eaf24edccd
RafaeelFreire <rafaelfreiree0110@gmail.com>
2021-06-26 16:18:20 -0300

    Stage 3

--- main.js
+++ main.js
@@ -18,18 +18,16 @@ for(const link of links){
   })
 }
 
-const header= document.querySelector('#header')
-const navHeight= header.offsetHeight
+function changeHeaderWhenScroll(){
+  const header= document.querySelector('#header')
+  const navHeight= header.offsetHeight
 
-window.addEventListener('scroll', function(){
   if(this.window.scrollY >= navHeight){
     header.classList.add('scroll')
-
   }else{
     header.classList.remove('scroll')
-
   }
-})
+}
 
 const swiper = new Swiper('.swiper-container', {
   slidesPerView:1,
@@ -52,12 +50,36 @@ const scrollReveal = ScrollReveal({
 scrollReveal.reveal(
   `#home .image, #home .text,
    #about .image, #about .text,
-   #services header, #services.card,
+   #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links
+   #contact .text, #contact .links,
+   footer .brand, footer .social
+
 
   `,
 { interval:100 })
+
+
+// back to top
+
+function backToTop(){
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  if(this.window.scrollY>=560){
+    backToTopButton.classList.add('show')
+
+  }else{
+    backToTopButton.classList.remove('show')
+  }
+
+
+}
+  
+window.addEventListener('scroll', function(){
+  changeHeaderWhenScroll()
+  backToTop()
+  
+})
 
   
 
